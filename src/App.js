ed1a69f5aa5b310d9de3d233fc0a999c04c3527e
Carlos Dartora <carlosdalsoler@gmail.com>
2022-01-13 23:52:59 -0300

    fix: corrige typo no Switch

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 // import logo from './trivia.png';
 import './App.css';
 import NotFound from './pages/NotFound';
-import GambeBody from './pages/GameBody';
+import GameBody from './pages/GameBody';
 import Login from './pages/Login';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/player" component={ GambeBody } />
+      <Route path="/player" component={ GameBody } />
       <Route component={ NotFound } />
     </Switch>
   );
