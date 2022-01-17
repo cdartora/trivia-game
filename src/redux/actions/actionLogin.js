6cb842691907318973108dae8ff8152fa1d3d752
Carlos Dartora <carlosdalsoler@gmail.com>
2022-01-17 15:49:23 -0300

    feat[token]: lida com token invalido

--- src/redux/actions/actionLogin.js
+++ src/redux/actions/actionLogin.js
@@ -28,16 +28,31 @@ export const saveQuestions = (questions) => ({
   questions,
 });
 
-export const loginHandler = (email) => (dispatch) => {
-  fetchToken()
-    .then(
-      ({ token }) => {
-        dispatch(userLogin(email));
-        dispatch(getRequestToken(token));
-        fetchQuestions(token)
-          .then((questions) => {
-            dispatch(saveQuestions(questions.results));
-          });
-      },
-    );
+export const loginHandler = (email) => async (dispatch) => {
+  dispatch(userLogin(email));
+};
+
+const validateToken = async (token, dispatch) => {
+  if (!token) {
+    token = await fetchToken();
+    dispatch(getRequestToken(token));
+  }
+};
+
+export const handleQuestions = () => async (dispatch, getState) => {
+  const { token } = getState();
+  const ERROR = 3;
+
+  validateToken(token, dispatch);
+
+  const { results, response_code: responseCode } = await fetchQuestions(token);
+
+  if (responseCode === ERROR) {
+    const newToken = await fetchToken();
+    dispatch(getRequestToken(token));
+    const newResults = await fetchQuestions(newToken).results;
+    dispatch(saveQuestions(newResults));
+  } else {
+    dispatch(saveQuestions(results));
+  }
 };
--- src/redux/helpers/fetchToken.js
+++ src/redux/helpers/fetchToken.js
@@ -1,10 +1,9 @@
 const API_URL = 'https://opentdb.com/api_token.php?command=request';
 
-const fetchToken = () => fetch(API_URL)
-  .then((response) => response
-    .json()
-    .then(
-      (json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)),
-    ));
+const fetchToken = async () => {
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return response.ok ? Promise.resolve(json) : Promise.reject(json);
+};
 
 export default fetchToken;
