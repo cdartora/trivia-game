f45c5baa25147377d1b33ee508a6f8d8f105adb3
Mauricio Lopes <mlopeesz@gmail.com>
2022-01-17 11:04:28 -0300

    Adicionado Header no Feedback - Req 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
+/* import { withRouter } from 'react-router'; */
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { email, name, score } = this.props;
     return (
       <div
         data-testid="feedback-text"
       >
+        <Header email={ email } name={ name } score={ score } />
         Feedback
       </div>
     );
   }
 }
 
-export default withRouter(Feedback);
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  score: state.player.score,
+  name: state.player.name,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  email: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
